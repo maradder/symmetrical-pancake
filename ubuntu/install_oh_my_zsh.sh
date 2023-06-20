49c0900a9e857910f6441df1a6155d8bb95fa056
Marcus <46413447+maradder@users.noreply.github.com>
2023-06-20 06:47:34 -0400

    Add plugins to the script (auto-suggestions, fonts-powerline)

--- ubuntu/install_oh_my_zsh.sh
+++ ubuntu/install_oh_my_zsh.sh
@@ -1,7 +1,7 @@
+#!/bin/bash
 
 # Update the system
 sudo apt update && sudo apt dist-upgrade -y
-
 
 # Ensure that you have the following packages installed
 sudo apt install build-essential curl file git -y
@@ -11,3 +11,12 @@ sudo apt install zsh -y
 
 # Check the Zsh version
 zsh --version
+
+# Install Oh My Zsh
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" --unattended
+
+# Install Powerline fonts
+sudo apt install fonts-powerline -y
+
+# Install zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
