3f00eb0577d61b1fe3846dd141d26c07d7410ef5
Marcus <46413447+maradder@users.noreply.github.com>
2023-06-20 06:44:01 -0400

    Create install_oh_my_zsh.sh

---
+++ ubuntu/install_oh_my_zsh.sh
@@ -0,0 +1,13 @@
+
+# Update the system
+sudo apt update && sudo apt dist-upgrade -y
+
+
+# Ensure that you have the following packages installed
+sudo apt install build-essential curl file git -y
+
+# Install Zsh
+sudo apt install zsh -y
+
+# Check the Zsh version
+zsh --version
